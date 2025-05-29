55d41b0ece2d5680fbcc94cc82c258a014ceefa6
= <=>
2025-05-29 03:52:50 -0700

    add button to get code and change image

--- script.js
+++ script.js
@@ -15,14 +15,14 @@ document.addEventListener("DOMContentLoaded", () => {
   typeEffect();
 });
 
-// Toggle mobile menu
-const menuToggle = document.getElementById("menu-toggle");
-const mobileMenu = document.getElementById("mobile-menu");
+    const menuToggle = document.getElementById('menu-toggle');
+    const mobileMenu = document.getElementById('mobile-menu');
 
-menuToggle.addEventListener("click", () => {
-  mobileMenu.classList.toggle("hidden");
-});
-// document.getElementById('contactForm').addEventListener('submit', function (e) {
-//   alert('Form submitted! If everything is set up correctly, you will receive an email shortly.');
-// });
+    menuToggle.addEventListener('click', () => {
+      mobileMenu.classList.toggle('hidden');
+      mobileMenu.classList.toggle('-translate-x-full');
+      mobileMenu.classList.toggle('translate-x-0');
+    });
+
+ 
 
